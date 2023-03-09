5925f86aaffce999ff5bcf0ca8ee26a1c7b4a620
shinter61 <shinter61@gmail.com>
2023-03-10 02:53:36 +0900

    First commit

---
+++ lua/commit-browse/init.lua
@@ -0,0 +1,13 @@
+local M = {}
+M.config = {
+
+}
+
+M.setup = function (args)
+end
+
+M.hello = function (args)
+  print('hello commit-browse!')
+end
+
+return M
---
+++ plugin/commit-browse.vim
@@ -0,0 +1,5 @@
+if exists('g:loaded_commit_browse')
+  finish
+endif
+let g:loaded_commit_browse = 1
+
